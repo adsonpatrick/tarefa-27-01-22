19d82bf1e52be3b93b41d771c9acda172f6b0969
Adson Patrique <95453978+adsonpatrick@users.noreply.github.com>
2022-01-27 22:06:27 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+//console.log(doocument.getElementById('novaTarefa'));
+//var inputNovaTarefa = document.getElementById('novaTarefa');
+//console.log(inputNovaTarefa.getAttribute('value'));
+
+var arrayTarefas = [];
+console.log(arrayTarefas, "array tarefas antes do push")
+
